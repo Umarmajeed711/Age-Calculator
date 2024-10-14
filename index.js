4e2ceb2a05a88d44b5edb0875f39002493607b3b
Umarmajeed711 <umarmajeed711@gmail.com>
2024-10-14 09:13:40 +0500

    Age Calculator

---
+++ index.js
@@ -0,0 +1,39 @@
+
+
+let todayDate = new Date("May 2, 2005")
+
+
+// let dayArr = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
+// let day = todayDate.getDay(); // its give a day in number between 0 to 6 , 0 = sunday & 6 = Saturday;
+  
+
+// let monthArr = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"]
+// let month = todayDate.getMonth(); // it gives a month in number between 0 to 11 , here 0 = January & 11 = December.
+
+
+// let date = todayDate.getDate(); // it gives a date between 0 to 31;
+
+
+// let year = todayDate.getFullYear(); // gives a current year
+
+
+// let hour = todayDate.getHours(); // it gives a 24 hours time , here 0 = 12am ,10 = 12am , 12 = 12pm ,14 = 2pm , 18 = 6pm  & 22 = 10pm;
+
+
+// let minute = todayDate.getMinutes(); // it gives a current minutes;
+
+
+
+// let second = todayDate.getSeconds(); // it gives a current seconds;
+
+
+// let milisecond = todayDate.getMilliseconds();
+
+
+// let mydate = `${date} ${dayArr[day]} ${month + 1} ${monthArr[month]} ${year} ${hour}:${minute}:${second}:${milisecond}`
+// console.log(mydate);
+
+
+
+userDate = todayDate.getFullYear()
+console.log(userDate);
